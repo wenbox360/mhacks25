e9e262464071d02e29ad2ba00f3153456444e716
tsha-256 <102328408+tsha-256@users.noreply.github.com>
2025-09-27 18:42:43 -0400

    should be final arduino code, needs test

--- boilerplate/boilerplate.c
+++ boilerplate/boilerplate.c
@@ -1,108 +1,79 @@
 #include <Servo.h>
 
 Servo turretServo;
-int turretCommand=1000;
-int irReading;
-int commandStep=20;
-int sweepDirection=1;
-
-
-//additional variables for tracking machineState
-int detectedFlag=0;
-int machineState=0; //0='normal' , 1='ACTIVE DETECTION', 2='upgrade state'
-int cycleCounter=0;
-
 
 void setup() {
-  turretServo.attach(8);
   Serial.begin(9600);
+#ifdef SERVO_PIN
+    turretServo.attach(SERVO_PIN);
+#endif
+}
+
+void servo_write(int angle){
+    turretServo.writeMicroseconds(angle);
+}
+
+int irSensorReading(){
+    return analogRead(IR_PIN);
+}
+
+void buzzer_duration(int duration){
+    tone(BUZZER_PIN, 1000);
+    delay(duration);
+    noTone(BUZZER_PIN);
+}
+
+void buzzer_on(){
+    tone(BUZZER_PIN, 1000);
+}
+
+void buzzer_off(){
+    noTone(BUZZER_PIN);
+}
+
+void led_on(){
+    digitalWrite(LED_PIN, HIGH);
+}
+
+void led_off(){
+    digitalWrite(LED_PIN, LOW);
 }
 
 void loop() {
 
-//////////////////////////////Process inputs///////
-irReading=analogRead(A0);
-if (irReading>300){detectedFlag=1;}
-else {detectedFlag=0;}
+//main loop code here
 
-
-/////////////////////////Decide what state the machine should be in /////
-if(detectedFlag==1){machineState=1;} //active detection
-
-else if(detectedFlag==0 && cycleCounter >0  ){machineState=2;} //high-alert mode
-
-else{ machineState=0;} //normal scanning
-
-///////////////////////// Process outputs based on state for this 30ms interval////
-if (machineState==0){  //Normal scanning
-  //turn off spotlight
-  digitalWrite(7,LOW);
-  //turn off alarm
-  noTone(6);
-  //Sweep turret normally
-  turretCommand=turretCommand+commandStep*sweepDirection;
-  //check sweep limits
-  if (turretCommand<=1000){sweepDirection=1;}
-  if (turretCommand>=2000){sweepDirection=-1;}
-  turretServo.writeMicroseconds(turretCommand);
-  //do nothing about cycleCounter
-  cycleCounter = cycleCounter;
-
+//read serial input
+if (Serial.available() > 0) {
+    int command = Serial.parseInt();
+    int param = Serial.parseInt();
 }
 
-
-if (machineState==1){  //Active detection
-  //Set spotlight on/off
-  digitalWrite(7,HIGH);
-
-  //Set alarm on/off
-  tone(6,500);
-    delay(500);
-    noTone(6);
-    delay(500);
-
-  //Sweep turret appropriately
-  turretCommand=turretCommand;
-  if (turretCommand<=1000){sweepDirection=1;}
-  if (turretCommand>=2000){sweepDirection=-1;}
-  turretServo.writeMicroseconds(turretCommand);
-
-  //Update cycleCounter
-  cycleCounter++;
-
+if(command == 2) { //Piezo command
+    buzzer_duration(param);
+    Serial.print("A");
+    command = 0; //reset command
 }
-
-
-if (machineState==2){ //upgrade state
-  //Set spotlight on/off
-  digitalWrite(7,HIGH);
-
-  //Set alarm on/off
-    tone(6,1000);
-    delay(500);
-    noTone(6);
-    delay(30);
-
-  //Sweep turret appropriately
-
-  turretCommand=turretCommand+commandStep*20*sweepDirection;
-  //check sweep limits
-  if (turretCommand<=1000){sweepDirection=1;}
-  if (turretCommand>=2000){sweepDirection=-1;}
-  turretServo.writeMicroseconds(turretCommand);
-  cycleCounter++;
-  //Update cycleCounter
-  if(cycleCounter>10)
-  {cycleCounter=0;
-  }
-
+else if (command == 20) { //SERVO command
+    servo_write(param);
+    Serial.print("A");
+    command = 0; //reset command
 }
-
-
-delay(15);
-noTone(6);  //// set tone to OFF after 15ms to create alarm sound if it was triggered.
-delay(15);
-//total delay still at 30ms
+else if (command == 30) { //LED command
+    if(param == 1) {
+        led_on();
+    } else {
+        led_off();
+    }
+    Serial.print("A");
+    command = 0; //reset command
+}
+ 
+//IR Sensor command
+int irValue = irSensorReading();
+Serial.print(40);
+Serial.print(irValue);
+Serial.print("\x00");
 
 //end of main loop
 }
