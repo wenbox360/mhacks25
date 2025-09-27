06806a9ebf0852cb343a8d93b1b47359341bde6f
tsha-256 <102328408+tsha-256@users.noreply.github.com>
2025-09-27 15:47:29 -0400

    added parser for boilerplate code

--- boilerplate/modifyBoilerplate.py
+++ boilerplate/modifyBoilerplate.py
@@ -9,3 +9,32 @@ def import_pins(input_json):
         pin_str += sensorMap['partId'].upper() +"_" + sensorMap['id'] + "_PIN "
         pin_str += str(sensorMap['pins'][0]) + " "
         pin_list.append(pin_str)
+    return pin_list
+
+def import_sensor_functions(input_json, function_map):
+    function_list = []
+    for sensorMap in input_json['mappings']:
+        function_list.append(function_map[sensorMap['partId']])
+    return function_list
+
+def make_unique(input_json, functions_json):
+    with open(functions_json, "r") as f:
+        functions = f.read()
+    with open(input_json, "r") as f:
+        input = f.read()
+    function_map = json.loads(functions)
+    pin_maps = json.loads(input)
+    pin_list = import_pins(pin_maps)
+    function_list = import_sensor_functions(pin_maps, function_map)
+    
+    with open("boilerplate/unique.c", "w") as f:
+        f.write("// Pin Definitions\n")
+        for pin in pin_list:
+            f.write(pin + "\n")
+        f.write("\n// Sensor Functions\n")
+        for function in function_list:
+            f.write(function + "\n\n")
+
+
+if __name__ == "__main__":
+    make_unique("boilerplate/pinMap.json", "boilerplate/sensorFunctions.json") # Example usage - probably need to modify pinMap.json
