c219697b5115b775b6187f13dee8d53670c387ae
Devansh Tayal <tayalde1@msu.edu>
2025-09-27 17:50:27 -0400

    arduino added
    - cant save without at least 1 now

--- frontend-wjsons/registry-server/.venv/bin/activate.fish
+++ frontend-wjsons/registry-server/.venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /Users/devli/Documents/mhacks/registry-server/.venv
+set -gx VIRTUAL_ENV /Users/devli/Documents/mhacks25/frontend-wjsons/registry-server/.venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
